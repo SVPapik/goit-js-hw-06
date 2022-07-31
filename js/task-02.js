11303a6663e4e159c41d9d74de4511bf920bf801
Vitaliy <fireflier87@gmail.com>
2022-07-31 23:59:28 +0300

    Update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const listElements = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredient;
+  listEl.classList.add("item");
+  return listEl;
+});
+
+ingredientsList.append(...listElements);
