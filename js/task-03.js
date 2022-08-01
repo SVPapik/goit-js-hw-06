1fff99c588b3aa09a9e91a1464d35672da5aa420
Vitaliy Lyakh <fireflier87@gmail.com>
2022-08-01 15:59:58 +0300

    task 03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const markupImg = images.map(({url, alt}) => {
+  return `<li><img src="${url}" alt="${alt}" width = "600px" height = "320px"></li>`}).join("");
+gallery.insertAdjacentHTML("afterbegin", markupImg);
+gallery.style.cssText = `display: flex;
+margin: 0;
+padding: 0;
+justify-content: center;
+gap: 15px;
+list-style: "";
+box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset; `
+
+
