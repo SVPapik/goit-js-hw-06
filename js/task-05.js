6019eb72eb2334860e0628f5186c36368983db3d
Vitaliy <fireflier87@gmail.com>
2022-08-05 01:33:03 +0300

    task 06

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,7 @@
-const nameInput = document.querySelector("#name-input")
-const nameOutput = document.querySelector("#name-output")
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-nameInput.addEventListener("input", (event) => {
-    nameOutput.textContent =  event.currentTarget.value == "" ? "Anonymous" : event.currentTarget.value;
+nameInput.addEventListener("input", (evt) => {
+  nameOutput.textContent =
+    evt.currentTarget.value === "" ? "Anonymous" : evt.currentTarget.value;
 });
