5ad0fd5ae176fa73530e82deb6f01bab95c0483e
Vitaliy Lyakh <fireflier87@gmail.com>
2022-08-05 17:13:51 +0300

    task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', changeBgColor);
+
+function changeBgColor() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  color.textContent = getRandomHexColor();
+}
